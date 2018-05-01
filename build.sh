7cfbef86b8ad4f05caa62172325a38bb9ea2cf59
dan-sf <fowler.dn@gmail.com>
2018-04-30 21:15:06 -0700

    Save db state and log output with volumes

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# @Note: I may need to generate an egg file here so that airflow has proper
+# links when docker looks into the airflow repo @InvestigateThis
+
 [[ -d incubator-airflow ]] && rm -rf incubator-airflow
 cp -r ../../git/incubator-airflow .
 docker build -t af-dev .
