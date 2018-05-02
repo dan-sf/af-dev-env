b8d832887d54f0f81c87c07a9f7c74e2890f9402
dan-sf <fowler.dn@gmail.com>
2018-05-01 20:56:13 -0700

    Add volumes in the celery docker compose

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 # @Note: I may need to generate an egg file here so that airflow has proper
 # links when docker looks into the airflow repo @InvestigateThis
 
 [[ -d incubator-airflow ]] && rm -rf incubator-airflow
+
+# # Is this needed? Is there a better way to do this??
+# pushd ../../git/incubator-airflow
+# python setup.py bdist_egg # This creates the egg file that links the source paths (research this more)
+# popd
+
 cp -r ../../git/incubator-airflow .
 docker build -t af-dev .
 rm -rf incubator-airflow
